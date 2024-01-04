32df975b2d37d58ccecc5abf490f5989d247fb1f
Wojciech Bancarzewski <w.bancarzewski@gmail.com>
2024-01-04 01:16:40 +0100

    Minor

--- lv_conf_edgetx.h
+++ lv_conf_edgetx.h
@@ -17,13 +17,9 @@
 #ifndef LV_CONF_EDGETX_H
 #define LV_CONF_EDGETX_H
 
-#if !defined(CONFIG_LV_TFT_DISPLAY_CONTROLLER_RA8875)
+
 #define LV_HOR_RES_MAX 480
 #define LV_VER_RES_MAX 320
-#else
-#define LV_HOR_RES_MAX 480
-#define LV_VER_RES_MAX 272
-#endif
 
 #include "main/edgetx/radio/src/lv_conf.h"
 
